a447414d58a60d73bda02820a59e3d4f8a7b0cbb
gnpashi <guynatan@gmail.com>
2023-03-28 17:19:17 +0300

    init db

--- __init__.py
+++ __init__.py
@@ -1,13 +1,24 @@
+import os
+import psycopg2
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
-posts = [
-    { 'author': 'Corey Schafer', 'title': 'Blog Post 1', 'content': 'First post content', 'date_posted': 'April 20, 2018' },
-    { 'author': 'Jane Doe', 'title': 'Blog Post 2', 'content': 'Second post content', 'date_posted': 'April 21, 2018' }
+def get_db_connection():
+    conn = psycopg2.connect(host='localhost',
+                            database='flask_test_dev',
+                            user=os.environ['DB_USERNAME'],
+                            password=os.environ['DB_PASSWORD'])
+    return conn
 
-]
-    
-@app.route("/")
-def hello_world():
-    return render_template("index.html", posts=posts)
 
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM books WHERE id = 1;')
+    books = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template('index.html', books=books)

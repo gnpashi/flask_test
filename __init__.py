e31bfd9fc00c77a3805232d0c39a4f1f9c8807a0
gnpashi <guynatan@gmail.com>
2023-03-28 12:25:18 +0300

    includes

--- __init__.py
+++ __init__.py
@@ -1,7 +1,13 @@
-from flask import (Flask, render_template)
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    { 'author': 'Corey Schafer', 'title': 'Blog Post 1', 'content': 'First post content', 'date_posted': 'April 20, 2018' },
+    { 'author': 'Jane Doe', 'title': 'Blog Post 2', 'content': 'Second post content', 'date_posted': 'April 21, 2018' }
+
+]
+    
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    return render_template("index.html", posts=posts)
+
---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+const navbarHamburger = document.querySelector('.navbar-burger');
+const navbarMenu = document.querySelector('.navbar-menu');
+navbarHamburger.addEventListener('click', () => {
+//   navbarHamburger.classList.toggle('is-active');
+  navbarMenu.classList.toggle('is-active');
+})
+
